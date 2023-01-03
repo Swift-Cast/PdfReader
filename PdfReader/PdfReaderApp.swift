bc709b51003eba4d0f8cc5606e7110d79d2e1119
Tinyfool <tinyfool@gmail.com>
2023-01-03 20:25:08 +0800

    Initial Commit

---
+++ PdfReader/PdfReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PdfReaderApp.swift
+//  PdfReader
+//
+//  Created by HaoPeiqiang on 2023/1/3.
+//
+
+import SwiftUI
+
+@main
+struct PdfReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
